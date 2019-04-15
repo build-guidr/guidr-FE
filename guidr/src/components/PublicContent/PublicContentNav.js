791e1a393ca87aecfda2e7386b10fa43157b4cae
tca88 <trishnaamin@gmail.com>
2019-04-15 18:33:30 -0400

    Convert Public and Gated Content Nav components to functional components.

--- guidr/src/components/PublicContent/PublicContentNav.js
+++ guidr/src/components/PublicContent/PublicContentNav.js
@@ -2,23 +2,23 @@ import React, { Component } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { createBrowserHistory } from "history";
 
-export default class PublicContentNav extends Component {
-  render() {
-    return (
-      <div>
-        <nav>
-          <NavLink to="/">Home</NavLink>
-          &nbsp;|&nbsp;
-          <NavLink to="/signup">SignUp</NavLink>
-          &nbsp;|&nbsp;
-          <NavLink to="/">Login</NavLink>
-          &nbsp;|&nbsp;
-        </nav>
-        <header>
-          <h1>Guidr</h1>
-          <h3>Your personalized outdoor outfitter portfolio</h3>
-        </header>
-      </div>
-    );
-  }
+function PublicContentNav() {
+  return (
+    <div>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        &nbsp;|&nbsp;
+        <NavLink to="/signup">SignUp</NavLink>
+        &nbsp;|&nbsp;
+        <NavLink to="/">Login</NavLink>
+        &nbsp;|&nbsp;
+      </nav>
+      <header>
+        <h1>Guidr</h1>
+        <h3>Your personalized outdoor outfitter portfolio</h3>
+      </header>
+    </div>
+  );
 }
+
+export default PublicContentNav;
