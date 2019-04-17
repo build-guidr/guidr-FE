c072cc8635568e6511d50694ce7bb1edb18d8478
chad kidd <ckiddwebdev@gmail.com>
2019-04-17 08:51:20 -0700

    Add Profile Form Up But needs work As well as Delete Function

--- guidr/src/components/GatedContent/Portfolio.js
+++ guidr/src/components/GatedContent/Portfolio.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import { Link, NavLink } from "react-router-dom";
+import { Route, Link, NavLink } from "react-router-dom";
 import GatedContentNav from "./GatedContentNav";
 import axios from 'axios'
+import AddProfile from "./AddProfile";
 
 export default class Portfolio extends Component {
   constructor() {
@@ -11,7 +12,7 @@ export default class Portfolio extends Component {
     };
   }
 
-  // create
+  // read
   componentDidMount() {
     axios 
     .get('https://ls-guidr.herokuapp.com/api/profile')
@@ -20,18 +21,40 @@ export default class Portfolio extends Component {
     })
     .catch(error => console.log(`unable to load Data`));
 }
+  update
+  updateProfile = updatedProfile => {
+    axios
+      .put( `https://ls-guidr.herokuapp.com/api/profile/${updatedProfile.id}`, updatedProfile)
+      .then(result => { console.log(result)
+        this.setState({ Profiles: result.data });
+        console.log(result);
+        // redirect
+        this.props.history.push(`/profile/${updatedProfile.id}`);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
 
   // delete
   deleteUser = id => {
     axios
-      .delete(`https://ls-guidr.herokuapp.com/api/users${id}`)
-      .then(result => 
-          this.setState({
-            user: result.data
-          })
-        )
+      .delete(`https://ls-guidr.herokuapp.com/api/profile/${id}`)
+      .then(result => {console.log("DELETE AXIOS",result.data)
+          this.setState({user: result.data}); 
+        })
       .catch(error => console.log(error))
   }
+
+  updateProfile = event => {
+    event.preventDefault(); console.log("EDIT HERE",this.user.id)
+    this.props.history.push(`/edit-profile/${this.user.id}`);
+  };
+
+  deleteProfile = event => {
+    event.preventDefault(); console.log("DELETE HERE",this.user.id)
+    this.deleteProfile(this.user.id);
+  };
   
   render() {
     return (
@@ -44,9 +67,13 @@ export default class Portfolio extends Component {
           {this.state.user.map(user => (
             <div key={user.id}>
               <p> <strong>Guidr:</strong> {user.first_name} {user.last_name}</p>
-              <p> <strong>Age:</strong> {user.years_of_exp}</p>
+              <p> <strong>Age:</strong> {user.age}</p>
               <p> <strong>About:</strong> {user.profile_text}</p>
-              <p> <strong>Certification:</strong> {user.certs}</p>
+              <p> <strong>Certification:</strong> {user.certs}</p>              
+              <p> <strong>Years of Experience:</strong> {user.years_of_exp}</p>
+              <button onClick={this.updateProfile}>Update Profile</button>
+              <button onClick={this.deleteProfile}>Delete Profile</button>
+              <Route render={props => <AddProfile {...props} addUser={this.addUser}/>} />
               <Link to={`/portfolio/${user.id}`}>
                 <p>Details</p>
               </Link>
